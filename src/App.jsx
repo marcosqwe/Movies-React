fe7ff08ac32ff201ba7a368cf3b686cc87431be1
MARCOS <marcoslopezsanchez@hotmail.com>
2022-03-03 13:29:04 -0600

    Esta incompleto, pasamos a Ubuntu

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MoviesGrid } from "./components/MoviesGrid";
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { MovieDetails } from "./Pages/MovieDetails";
+import { LandingPage } from  "./Pages/LandingPage";
+
+export function App() {
+  return (
+    <Router>
+      <header>
+        <Link to="/"><h1 className={styles.title}>Movies</h1>
+        </Link>
+      </header>
+      <main>
+        <Routes>
+          <Route exact path="/movie">
+            <MovieDetails />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Routes>
+      </main>
+    </Router>
+  );
+}
