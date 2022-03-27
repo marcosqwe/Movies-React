38a7213a6710241117f8122bbcc57fc09f7b335e
MARCOS <marcoslopezsanchez@hotmail.com>
2022-03-27 09:36:00 -0600

    Dura la situación
    Agregamos utils, conexión con the-movie-db

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ export function  MovieCard({ movie }) {
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
     return ( 
     <li className={styles.movieCard}>
-        <Link to={"/movies/" + movie.Id}>
+        <Link to={"/movies/" + movie.id}>
         <img 
         width={230}
         height={345}
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../utils/httpClient";
+import styles from "./MovieDetails.module.css";
+
 export function MovieDetails() {
-    return <div>Details</div>;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState (null);
+
+  useEffect (() => {
+    get ( "/movie/" + movieId).then(data => {
+      setMovie(data)
+    })
+  }, [movieId])
+
+  if (!movie){
+    return null;
+  }
+
+  const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+  return (
+    <div className={styles.detailsContainer}>
+      <img
+        className={`${styles.col} ${styles.movieImage}`}
+        src={imageUrl}
+        alt={movie.title}
+      />
+      <div className={`${styles.col} ${styles.movieDetails}`}>
+        <p className={styles.firstItem}>
+          <strong>Title:</strong> {movie.title}
+        </p>
+        <p>
+          <strong>Genres:</strong>{" "}
+          {movie.genres.map((genre) => genre.name).join(", ")}
+        </p>
+        <p>
+          <strong>Descripcion:</strong> {movie.overview}
+        </p>
+      </div>
+    </div>
+  );
 }
